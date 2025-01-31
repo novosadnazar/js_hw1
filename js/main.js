0339092f9d52bb043d4587bf2db928d9be8aae55
novosadnazar <163895331+novosadnazar@users.noreply.github.com>
2025-01-31 21:28:42 +0200

    full

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const age = 14;
+console.log(age);
+const name = "Nazar";
+console.log(name)
+const isStudent = true;
+console.log(isStudent)
+const myString = "Лінь вбиває вашу ціль";
+console.log(myString);
+const myNumber = 15 + 10;
+console.log(myNumber);
+const myNull = null;
+console.log(myNull);
